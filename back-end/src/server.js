bf9cff94ee64cbf2908595292f3db5803a171061
nayena <heidynaranjo@brandeis.edu>
2025-12-20 20:05:53 -0500

    feat(article  upvote api): api to update the votes

---
+++ back-end/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'; 
+
+
+
+const articleInfo = [
+    { name : 'learn-react' , upvotes: 0 },
+    { name : 'learn-node' , upvotes: 0 },
+    { name : 'mongodb' , upvotes: 0 }, 
+]
+
+const app = express();
+app.use(express.json());
+
+app.post('/api/articles/:name/upvote', (req, res) =>{
+    const article = articleInfo.find(a => a.name === req.params.name);
+    article.upvotes += 1 ;  
+    res.send('success! The article ' + req.params.name + 'now has ' + article.upvotes + ' votes') ; 
+}); 
+
+app.listen(8000, function() {
+    console.log('Server is running in port 8000');
+}); 
+
